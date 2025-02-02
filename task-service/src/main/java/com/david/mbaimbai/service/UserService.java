9cd94133a8bd385f23f31f9625389e91890f8c9e
David Mbaimbai <dmbaimbai@csod.com>
2025-02-02 13:21:32 +0200

    Initial Commit

--- task-service/src/main/java/com/david/mbaimbai/service/UserService.java
+++ task-service/src/main/java/com/david/mbaimbai/service/UserService.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "USER-SERVICE", url = "http://localhost:9888")
 public interface UserService {
-    @GetMapping("/user/profile")
+    @GetMapping("/users/profile")
     public UserDto getUserProfile(@RequestHeader("Authorization") String jwt);
 }
