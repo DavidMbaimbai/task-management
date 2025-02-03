69aeca5bb95d3a6fa163d3d29c7380cb35ccd880
David Mbaimbai <dmbaimbai@csod.com>
2025-02-03 21:22:15 +0200

    Refactored Task

--- task-service/src/main/java/com/david/mbaimbai/entity/Task.java
+++ task-service/src/main/java/com/david/mbaimbai/entity/Task.java
@@ -2,10 +2,7 @@ package com.david.mbaimbai.entity;
 
 import com.david.mbaimbai.enums.TaskStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,9 +17,12 @@ public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Column(name = "title", unique = true, nullable = false)
     private String title;
+    @Column(name = "description", unique = true, nullable = false)
     private String description;
     private Long assignedUserId;
+    @Column(name = "taskStatus", unique = true, nullable = false)
     private TaskStatus taskStatus;
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime deadline;
